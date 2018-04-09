04332ae8e376098b01561eb40a6f0d255dfe5af6
Colin PIREYRE <cpireyre@student.42.fr>
2018-04-09 17:21:24 +0200

    Norm fix.

--- ft_lstappend.c
+++ ft_lstappend.c
@@ -6,11 +6,11 @@
 /*   By: cpireyre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 16:04:14 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/04/09 16:45:34 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/04/09 17:20:48 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"	
+#include "libft.h"
 
 void	ft_lstappend(t_list **node, t_list *addend)
 {
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,11 +6,11 @@
 /*   By: cpireyre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 16:30:21 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/04/09 17:10:51 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/04/09 17:20:57 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"	
+#include "libft.h"
 
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {

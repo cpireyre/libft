0b7e6d8bd1edaec75c1839c01728f6ca6de8f8c0
Colin PIREYRE <cpireyre@student.42.fr>
2018-05-13 09:29:16 +0200

    added ft_strrealloc

---
+++ ft_strrealloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrealloc.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/13 09:20:58 by cpireyre          #+#    #+#             */
+/*   Updated: 2018/05/13 09:23:48 by cpireyre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_strrealloc(char *str, size_t size)
+{
+	void	*realloc;
+
+	realloc = ft_strnew(size);
+	if (!realloc)
+		return (NULL);
+	if (str && *str)
+	{
+		ft_strcpy(realloc, str);
+		ft_strdel(&str);
+	}
+	return (realloc);
+}

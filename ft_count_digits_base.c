2610dc7e9bbca45d1bb5ddf3fbf213238b949081
Colin PIREYRE <cpireyre@student.42.fr>
2018-06-14 07:54:36 +0200

    extra space for minus sign when necessary

--- ft_count_digits_base.c
+++ ft_count_digits_base.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 11:54:06 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/06/13 11:54:07 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/06/14 07:43:46 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_count_digits_base(long long n, int base)
 {
 	int	count;
 
-	count = 1;
+	count = 1 + ((n < 0) ? 1 : 0);
 	while ((n = (n / base)))
 		count++;
 	return (count);

3b2bcad2e44199441f77753e276a5c7b70abfaa4
Colin PIREYRE <cpireyre@student.42.fr>
2018-09-02 12:38:09 +0200

    null fix

--- ft_print_int_tab.c
+++ ft_print_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 11:52:23 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/09/01 11:56:31 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/09/01 12:08:42 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_print_int_tab(int *tab, int size)
 {
 	int	i;
 
+	if (size < 1)
+		ft_printf("Empty tab.\n");
 	i = -1;
 	while (++i < size)
 		ft_printf("tab[%d] = %d\n", i, tab[i]);

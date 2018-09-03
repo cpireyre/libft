045b0a50a79239fa0ae02e5a29f632c1c74c4870
Colin PIREYRE <cpireyre@student.42.fr>
2018-09-03 09:31:54 +0200

    decided to cowboy it...

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 15:05:03 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/05/13 08:59:26 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/09/02 16:54:39 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strnew(size_t len)
 {
 	char	*new;
 
-	if (!(new = ft_memalloc(sizeof(char) * (len + 1))))
-		return (NULL);
+	new = ft_memalloc(sizeof(char) * (len + 1));
 	return (new);
 }
--- ft_strrealloc.c
+++ ft_strrealloc.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/13 09:20:58 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/05/15 07:51:57 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/09/02 16:54:39 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ char	*ft_strrealloc(char *str, size_t size)
 	char	*realloc;
 
 	realloc = ft_strnew(size);
-	if (!realloc)
-		return (NULL);
 	if (str)
 	{
 		ft_strcpy(realloc, str);

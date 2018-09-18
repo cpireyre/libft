a1f806ca98fc8430dd7b6d005827a86bc50fc0d4
Colin PIREYRE <cpireyre@student.42.fr>
2018-09-18 09:16:37 +0200

    buffer too big was misbehaving. pipe it down

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/04 12:33:30 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/09/03 12:45:32 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/09/18 09:02:33 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE	8192
+# define BUFF_SIZE	1024
 # define SPLIT		'\n'
 # include "libft.h"
 # include <stdlib.h>

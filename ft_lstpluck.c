5c0954c696a52617a5b071d5dc983821c151f0b4
Colin PIREYRE <cpireyre@student.42.fr>
2018-12-14 15:05:37 +0100

    ?

--- ft_lstpluck.c
+++ ft_lstpluck.c
@@ -5,7 +5,9 @@ void	ft_lstpluck(t_list **list, t_list **to_del, void (*del)(void *, size_t))
 	t_list	*tmp;
 	t_list	*new_next;
 
-	if (*list == *to_del)
+	if (!list || !*list || !to_del || !*to_del)
+		return ;
+	else if (*list == *to_del)
 	{
 		tmp = (*list)->next;
 		ft_lstdelone(to_del, del);

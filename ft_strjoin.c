fe9127746ec9be701312cd75c41a95c9d8d5f605
Colin PIREYRE <cpireyre@student.42.fr>
2018-12-24 14:08:15 +0100

    look @edwardjs002 no leaks

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/15 15:29:54 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/05/05 12:57:24 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/12/24 14:07:22 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	str = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
-	if (!str)
-		return (NULL);
 	str = ft_strcpy(str, (char*)s1);
 	str = ft_strcat(str, (char*)s2);
 	return (str);

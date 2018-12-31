7fc6819ca053216babcdb8375ed322e8c5994bb0
Colin PIREYRE <cpireyre@student.42.fr>
2018-12-31 15:37:15 +0100

    took out redundant protection

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/15 09:31:48 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/11/08 08:12:38 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/12/31 15:37:04 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ char	*ft_itoa_base(int n, int base)
 	nbr = (long)n * (n < 0 ? -1 : 1);
 	while (n /= base)
 		digits++;
-	if (!(str = ft_strnew(digits)))
-		return (NULL);
+	str = ft_strnew(digits);
 	if (sign)
 		str[0] = '-';
 	while (digits-- - sign)

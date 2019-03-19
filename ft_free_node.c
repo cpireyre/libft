992238fb74e20bc213d57d117c71c9a9a5117647
Colin PIREYRE <cpireyre@student.42.fr>
2019-03-19 14:35:17 +0100

    norm made me do it

--- ft_free_node.c
+++ ft_free_node.c
@@ -6,14 +6,15 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 14:32:22 by cpireyre          #+#    #+#             */
-/*   Updated: 2019/03/19 14:33:55 by cpireyre         ###   ########.fr       */
+/*   Updated: 2019/03/19 14:35:06 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void	ft_free_node(void *ptr, size_t size __attribute__((unused)))
+void	ft_free_node(void *ptr, size_t size)
 {
+	(void)size;
 	ft_memdel(&ptr);
 }

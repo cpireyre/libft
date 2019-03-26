cad0d2a59dd52b2732f2049fc75cf61ab27297e2
cpireyre <cpireyre@student.42.fr>
2019-03-26 11:13:56 +0100

    More chunking optimizations. @nihuynh Setting 65556 * 32 bytes went from like 5ms to 1ms.

--- src/mem/ft_memcpy.c
+++ src/mem/ft_memcpy.c
@@ -14,7 +14,19 @@
 
 typedef	int64_t	__attribute__((__may_alias__))	i64;
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+static void	write_sixtyfour_bytes(i64 *lpdst, i64 *lpsrc)
+{
+		lpdst[0] = lpsrc[0];
+		lpdst[1] = lpsrc[1];
+		lpdst[2] = lpsrc[2];
+		lpdst[3] = lpsrc[3];
+		lpdst[4] = lpsrc[4];
+		lpdst[5] = lpsrc[5];
+		lpdst[6] = lpsrc[6];
+		lpdst[7] = lpsrc[7];
+}
+
+void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	t_byte 		*pdst;
 	t_byte 		*psrc;
@@ -23,15 +35,12 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 
 	lpdst = (i64*)dst;
 	lpsrc = (i64*)src;
-	while (n > 32)
+	while (n > 64)
 	{
-		lpdst[0] = lpsrc[0];
-		lpdst[1] = lpsrc[1];
-		lpdst[2] = lpsrc[2];
-		lpdst[3] = lpsrc[3];
-		lpdst += 4;
-		lpsrc += 4;
-		n -= 32;
+		write_sixtyfour_bytes(lpdst, lpsrc);
+		lpdst += 8;
+		lpsrc += 8;
+		n -= 64;
 	}
 	pdst = (t_byte*)lpdst;
 	psrc = (t_byte*)lpsrc;
--- src/mem/ft_memset.c
+++ src/mem/ft_memset.c
@@ -12,11 +12,38 @@
 
 #include "mem.h"
 
-void	*ft_memset(void *b, int c, size_t len)
-{
-	t_byte *pb;
+typedef	int64_t	__attribute__((__may_alias__))	i64;
 
-	pb = (t_byte*)b;
+static void	write_sixtyfour_bytes(i64 *bigptr, i64 pattern)
+{
+	bigptr[0] = pattern;
+	bigptr[1] = pattern;
+	bigptr[2] = pattern;
+	bigptr[3] = pattern;
+	bigptr[4] = pattern;
+	bigptr[5] = pattern;
+	bigptr[6] = pattern;
+	bigptr[7] = pattern;
+}
+
+void		*ft_memset(void *b, int c, size_t len)
+{
+	i64		*bigptr;
+	i64		pattern;
+	t_byte	*pb;
+
+	bigptr = (i64*)b;
+	pattern = (t_byte)c;
+	pattern |= pattern << 8;
+	pattern |= pattern << 16;
+	pattern |= pattern << 32;
+	while (len > 64)
+	{
+		write_sixtyfour_bytes(bigptr, pattern);
+		bigptr += 8;
+		len -= 64;
+	}
+	pb = (t_byte*)bigptr;
 	while (len--)
 		*pb++ = (t_byte)c;
 	return (b);

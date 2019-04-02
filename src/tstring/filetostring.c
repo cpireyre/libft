9a1a0525b2554e2307aa99c3f7b077f42499e37b
Colin PIREYRE <cpireyre@student.42.fr>
2019-04-02 14:22:32 +0200

    Refactored error handling in filetostring

--- src/tstring/filetostring.c
+++ src/tstring/filetostring.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 15:48:15 by cpireyre          #+#    #+#             */
-/*   Updated: 2019/04/02 14:01:47 by cpireyre         ###   ########.fr       */
+/*   Updated: 2019/04/02 14:13:47 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
-**	new_size can never equal MALLOC_ERR since
-**	one is even and the other is odd
-*/
-
-static ssize_t	copy_array(char **old_ptr, ssize_t curr, ssize_t data_amount)
+static int	copy_array(char **old_ptr, ssize_t *curr, ssize_t data_amount)
 {
 	char	*new_ptr;
-	ssize_t	new_size;
 
-	new_size = curr * 2;
-	new_ptr = malloc(sizeof(char) * new_size);
+	*curr *= 2;
+	new_ptr = malloc(sizeof(char) * *curr);
 	if (!new_ptr)
 	{
 		ft_strdel(old_ptr);
@@ -38,29 +32,31 @@ static ssize_t	copy_array(char **old_ptr, ssize_t curr, ssize_t data_amount)
 	ft_memcpy(new_ptr, *old_ptr, sizeof(char) * data_amount);
 	ft_strdel(old_ptr);
 	*old_ptr = new_ptr;
-	return (new_size);
+	return (SUCCESS);
 }
 
 static int		fill_buffer(int fd, t_str *buf)
 {
 	ssize_t	bytes_read;
 	ssize_t	space;
+	int		errcode;
 
 	space = BUFSIZ;
 	while (1)
 	{
 		bytes_read = read(fd, buf->str + buf->length, BUFSIZ);
-		if (bytes_read <= 0)
+		if (bytes_read == -1)
 		{
-			if (bytes_read != 0)
-				ft_strdel(&buf->str);
-			return (bytes_read);
+			ft_strdel(&buf->str);
+			return (READ_ERR);
 		}
+		else if (bytes_read < BUFSIZ)
+			return (SUCCESS);
 		buf->length += bytes_read;
 		if (space - buf->length < BUFSIZ)
 		{
-			space = copy_array(&buf->str, space, buf->length);
-			if (space == MALLOC_ERR)
+			errcode = copy_array(&buf->str, &space, buf->length);
+			if (errcode == MALLOC_ERR)
 				return (MALLOC_ERR);
 		}
 	}
